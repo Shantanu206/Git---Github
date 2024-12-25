377f6712c17709ac0757a11b74359a8478837621
*first <*akotkarshantanu92@gmail.com*>
2024-12-25 12:38:38 +0530

    Data Types

---
+++ DataType.js
@@ -0,0 +1,17 @@
+//"use Strict"; // treat all Js code as newer version
+
+let name = "Shantanu" //String
+let age = "21" //Integer
+let isLoggedIn = false  //boolean
+
+let State;
+
+//number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => Standalone value
+//undefind => 
+//symbol => unique
+//object 
+console.log(typeof undefined);
