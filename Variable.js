c7d09d34fbe9bdd3c4fe47ba94e74463391f0537
*first <*akotkarshantanu92@gmail.com*>
2024-12-25 12:39:27 +0530

    Js Variable

---
+++ Variable.js
@@ -0,0 +1,14 @@
+const accountId = 100383
+let accountEmail = "shanu@gmail.com"
+var accountPassword = "2020"
+accountCity = "Loni"
+acccountState = "Mahanrshtra"
+
+//prefer not to use var because is block scope and  function scope
+
+//Variable Change
+accountEmail = "shantanu@gmail.com"
+accountPassword = "2082005"
+accountCity = "Amrawti"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, acccountState])
